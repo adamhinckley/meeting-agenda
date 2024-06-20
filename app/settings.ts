1e43d94a6b57656fdcaab87dc4642369090435f4
Adam Hinckley <adam.hinckley@clickbank.com>
2024-06-19 23:20:26 -0500

    Initial commit

---
+++ app/settings.ts
@@ -0,0 +1,34 @@
+const isTestimonyMeeting = false;
+
+const presiding = "Bishop Rob Dickson";
+const conducting = "Brother Rick Jensen";
+const musicLeader = "Sister Vanessa Pettus";
+const accompanist = "Sister Wendy Infanger";
+const openingHymn = "O God, the Eternal Father #175";
+const openingPrayer = "By Invitation";
+const sacramentHymn = "O God, the Eternal Father #175";
+const speakerOne = "Sister Rachel Pettus";
+const specialMusicalNumber = "";
+const intermediateHymn = "I am a child of God #301";
+const speakerTwo = "Sister Amy Bengal";
+const closingHymn = "Our Father, by Whose Name #296";
+const closingPrayer = "By Invitation";
+const date = "June 16, 2024";
+
+export const settings = {
+  isTestimonyMeeting,
+  presiding,
+  conducting,
+  musicLeader,
+  accompanist,
+  openingHymn,
+  openingPrayer,
+  sacramentHymn,
+  speakerOne,
+  specialMusicalNumber,
+  intermediateHymn,
+  speakerTwo,
+  closingHymn,
+  closingPrayer,
+  date,
+};
