321e67b69e626ae129f1d0cfd51ec77a35e485b2
Adam Hinckley <adam.hinckley@clickbank.com>
2024-06-21 15:25:37 -0500

    Update title and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Florence Ward Sacrament Meeting",
+  title: "Agenda",
   description: "Sacrament Meeting Agenda for Florence Ward",
 };
 
