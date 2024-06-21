7481bc22628bca35ac896a7089aeb8e1bc56f6ef
Adam Hinckley <adam.hinckley@clickbank.com>
2024-06-21 15:38:18 -0500

    Agenda -> Ward Program

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Agenda",
+  title: "Ward Program",
   description: "Sacrament Meeting Agenda for Florence Ward",
 };
 
