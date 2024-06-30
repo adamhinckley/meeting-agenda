c7fe5e3e38d82c540e65ea165d949980d32f6f95
Jewkes <gingersnapjaz@gmail.com>
2024-06-30 16:11:05 -0500

    Update settings.ts
    July 7th Program

--- app/settings.ts
+++ app/settings.ts
@@ -2,19 +2,20 @@
 // this app has configuration that will not allow images to be used unless they are from 'churchofjesuschrist.org.
 // We can add more domains if it's necessary.
 const imageUrl =
-	'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/Trujillo-Peru-Temple-2.jpg';
+	'https://www.churchofjesuschrist.org/media/image/fort-collins-temple-sunset-9ea119c?lang=eng&collectionId=2472c374d8394f768daa301e60550010',
+ /* https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/Trujillo-Peru-Temple-2.jpg';  */
 
 // agenda
-const isTestimonyMeeting = false;
+const isTestimonyMeeting = true;
 const presiding = 'Bishop Rob Dickson';
-const conducting = 'Brother Rick Jensen';
+const conducting = 'Brother Justin Lowry';
 const musicLeader = 'Sister Vanessa Pettus';
 const accompanist = 'Sister Wendy Infanger';
-const openingHymn = '26';
-const openingHymnTitle = 'Joseph Smith’s First Prayer';
+const openingHymn = '265';
+const openingHymnTitle = 'Arise, O God, and Shine';
 const openingPrayer = 'By Invitation';
-const sacramentHymn = '180';
-const sacramentHymnTitle = 'Father in Heaven We Do Believe';
+const sacramentHymn = '169';
+const sacramentHymnTitle = 'As Now We Take the Sacrament';
 const blockOne = [
 	{
 		title: 'Speaker',
@@ -34,39 +35,41 @@ const blockTwo = [
 		content: 'Testimonies as requested',
 	},
 ];
-const closingHymn = '78';
-const closingHymnTitle = 'God of Our Fathers, Whose Almighty Hand';
+const closingHymn = '156';
+const closingHymnTitle = 'Sing We Now at Parting';
 const closingPrayer = 'By Invitation';
 
 // Announcements
 const wardAnnouncements = [
 	'The Addiction Recovery Program (ARP) contact Jim & April Sturtevant (530) 906-3289.',
 	'Empty Nesters Family Home Evenings Mondays at 6:00 PM in R.S. Room. Potluck to follow.',
-	'Ward Choir practice rehearsals on the 1st (6-5) & 3rd (6-19) Wednesday of each month at 6:45 PM.',
-	'Ward 5th Sunday Potluck after church June 30th. Bring something to share.',
-];
+	'Ward Choir practice rehearsals on the 1st (7-3) & 3rd (7-17) Wednesday of each month at 6:45 PM.',
+	];
 
 const reliefSocietyLessons = [
 	{
 		text: 'Lessons:',
 	},
 	{
-		link: 'https://www.churchofjesuschrist.org/study/general-conference/2024/04/54held?lang=eng',
-		text: 'June 23 – Elder Held: “Opposition in All Things”',
-	},
-	{
 		link: 'https://www.churchofjesuschrist.org/study/general-conference/2024/04/15dushku?lang=eng',
 		text: 'July 14 - Elder Dushku: “Pillars and Rays”',
+	},
+	{
+		link: 'https://www.churchofjesuschrist.org/study/general-conference/2024/04/53stevenson?lang=eng',
+		text: 'July 28 - Elder Stevenson: “Bridging the Two Great Commandments”',
 	},
 	{
 		text: 'Activities:',
 	},
 	{
-		link: '',
 		text: 'July 3 – R.S. Sisters Luncheon 12:00 at the church.',
 	},
 	{
 		text: 'July 18 at 7:00 PM Combined EQ/RS Game Night. Bring a game to share or just come have fun. Child care provided.',
+	},
+	{
+		link: 'https://docs.google.com/forms/d/e/1FAIpQLSdot2ycfJdTxbMuKeDKwCF8r7zSi_RHy0Z1xhvjGRK4Sz_FhQ/viewform?pli=1',
+		text: 'July 27 - Single Sisters age 18 & up A Day at the Temple Sign up and join us at the Memphis Tennessee Temple',
 	},
 ];
 
@@ -75,47 +78,49 @@ const priesthoodLessons = [
 		text: 'Lessons:',
 	},
 	{
-		link: 'https://www.churchofjesuschrist.org/study/general-conference/2024/04/35uchtdorf?lang=eng',
-		text: 'June 23 – "A Higher Joy" - Elder Dieter F. Uchtdorf',
-	},
-	{
 		link: 'https://www.churchofjesuschrist.org/study/general-conference/2024/04/53stevenson?lang=eng',
 		text: 'July 14 – "Bridging the Two Great Commandments" - Elder Gary E. Stevenson',
+	},
+	{
+		link: 'https://www.churchofjesuschrist.org/study/general-conference/2024/04/57nelson?lang=eng',
+		text: 'July 28 - "Rejoice in the Gift of Priesthood Keys" - President Russell M Nelson',
 	},
 	{
 		text: 'Activities:',
 	},
 	{
-		link: '',
+		text: 'July 18 at 7:00 PM Combined EQ/RS Game Night. Bring a game to share or just come have fun. Child care provided.',
+	},
+	{
 		text: 'July 19 – EQ Presidency Temple Trip to Nashville Temple – Depart Ward building at 8:30 AM. All temple recommend holders welcome.',
 	},
 ];
 
 const sundaySchoolLessons = [
 	{
-		link: '',
-		text: 'May 27–June 2: Mosiah 25–28',
+		link: 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/27?lang=eng',
+		text: 'July 1–7: Alma 17–22',
 	},
 	{
-		link: '',
-		text: 'June 3–9: Mosiah 29–Alma 4',
+		link: 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/28?lang=eng',
+		text: 'July 8–14: Alma 23–29',
 	},
 	{
-		link: '',
-		text: 'June 10–16: Alma 5–7',
+		link: 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/29?lang=eng',
+		text: 'July 15–21: Alma 30–31',
 	},
 	{
-		link: 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/25?lang=eng',
-		text: 'June 17–23: Alma 8–12',
+		link: 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/30?lang=eng',
+		text: 'July 22–28: Alma 32–35',
 	},
 	{
-		link: 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/26?lang=eng',
-		text: 'June 24–30: Alma 13–16',
+		link: 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/31?lang=eng',
+		text: 'July 29–August 4: Alma 36–38',
 	},
 ];
 
 const primaryAnnouncements = [
-	'Primary Activity Night on the 2nd (7-10) & 4th (6-26) Wednesday at 6:30 PM.',
+	'Primary Activity Night on the 2nd (7-10) & 4th (7-24) Wednesday at 6:30 PM.',
 ];
 
 //page three
