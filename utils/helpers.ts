42dfccefa8a9768e383adebbfb2d55602d4f9fe5
Adam Hinckley <adam.hinckley@clickbank.com>
2024-07-28 08:35:26 -0500

    simplify regex

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -6,8 +6,7 @@ export const getHymnLink = (hymnNumber: string, hymnTitle: string, closingHymnLi
 	const title = hymnTitle
 		?.toLowerCase()
 		.trim()
-		.replace(/[^a-z\s]/g, '')
-		.replace(/\s/g, '-');
+		.replace(/[^a-z]+/g, '-');
 
 	return Number(hymnNumber) < 1000
 		? `https://www.churchofjesuschrist.org/study/manual/hymns/${title}?lang=eng`
