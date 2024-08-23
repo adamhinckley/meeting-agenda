84e7a2137fe1004842efbbb42f33ed9485df8631
Adam Hinckley <adam.hinckley@clickbank.com>
2024-08-22 21:46:40 -0500

    Update version

--- app/announcements.tsx
+++ app/announcements.tsx
@@ -1,9 +1,34 @@
+'use client';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { useAppContext } from '@/context/AppContext';
-import { Divider } from '@mui/material';
-import type { Announcement, Lesson } from '@/utils/defaultContent';
+import { useEffect } from 'react';
+
+const announcementStyles = css`
+	hr {
+		border: 0;
+		border-top: 1px solid #ccc;
+		margin: 12px 0;
+	}
+
+	a {
+		color: #1e40af;
+		text-decoration: underline;
+	}
+`;
 
 const Announcements = () => {
 	const { content } = useAppContext();
+
+	useEffect(() => {
+		if (typeof document !== 'undefined') {
+			const announcementEl = document.getElementById('announcement') as HTMLElement;
+			// add the html from content.announcements to the announcement element
+			if (announcementEl) {
+				announcementEl.innerHTML = content.announcements as string;
+			}
+		}
+	}, [content.announcements]);
 
 	if (Object.keys(content).length === 0) {
 		return null;
@@ -17,82 +42,7 @@ const Announcements = () => {
 		return null;
 	}
 
-	return (
-		<div className="mx-4">
-			{announcementsAndLessons.map((itemToNarrow, index) => {
-				const item = itemToNarrow as Lesson | Announcement;
-
-				if (item.type === 'lesson' && item.lessons?.length) {
-					return (
-						<div key={index}>
-							<h3 className="text-base font-semibold text-center">{item.title}</h3>
-							<ul>
-								{item.lessons?.map((lesson, index) =>
-									lesson.link ? (
-										<a
-											href={lesson.link}
-											key={index}
-											className="underline  text-blue-800"
-										>
-											<li key={index}>{lesson.text}</li>
-										</a>
-									) : (
-										<li key={index} style={{ wordWrap: 'break-word' }}>
-											{lesson.text}
-										</li>
-									),
-								)}
-							</ul>
-							<Divider className="my-4" />
-						</div>
-					);
-				}
-				if (item.type === 'announcement' && item.text?.length) {
-					return (
-						<div key={index}>
-							<h3 className="text-base font-semibold text-center">{item.title}</h3>
-							<ul>
-								{item.text?.map((text: string, index: number) => (
-									<li key={index} style={{ wordWrap: 'break-word' }}>
-										{text}
-									</li>
-								))}
-							</ul>
-							<Divider className="my-4" />
-						</div>
-					);
-				}
-			})}
-			<h3 className="text-base font-semibold text-center">Family History Corner</h3>
-			<ul>
-				<li>
-					Our readings in Come, Follow Me this week are in a time of war between the
-					Nephites and the Lamanites. What do you know of your ancestors and relatives who
-					may have fought to preserve freedoms and way of life? What discovery clues do
-					their records contain?{' '}
-				</li>
-				<li>
-					While working with my mother on our own family history, it was the military
-					records of a family member which allowed us to verify and confirm several pieces
-					of his family's story. The records verified his various residences, confirmed
-					that he was single throughout his life, and mentioned his next of kin so that we
-					could clearly see that it was indeed our family member and not another soldier
-					with the same name. (Lani Jackson)
-				</li>
-				<li>
-					The{' '}
-					<a
-						href="https://www.familysearch.org/en/wiki/United_States_Military_Records"
-						target="_blank"
-						className="underline  text-blue-800"
-					>
-						FamilySearch wiki
-					</a>{' '}
-					is one source that can help you research these records
-				</li>
-			</ul>
-		</div>
-	);
+	return <div className="mx-4" id="announcement" css={announcementStyles}></div>;
 };
 
 export default Announcements;
---
+++ components/editor/SaveButton.tsx
@@ -0,0 +1,77 @@
+'use client';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { createClient } from '@/utils/supabase/client';
+import { useAppContext } from '../../context/AppContext';
+import { useState, useContext } from 'react';
+import LoadingButton from '@mui/lab/LoadingButton';
+
+const styles = css`
+	.save-button {
+		margin: 12px 0;
+		max-width: 80px;
+		background-color: #1976d2;
+		box-shadow: none;
+	}
+`;
+
+const SaveButton = () => {
+	const supabase = createClient();
+	const { content, setContent, editorContentRef } = useAppContext();
+	const [saving, setSaving] = useState(false);
+	const [error, setError] = useState(false);
+
+	const handleSave = async () => {
+		try {
+			setSaving(true);
+			// save the announcement content before saving the entire content
+			const contentToSave = { ...content, announcements: editorContentRef.current };
+			SVGTextContentElement;
+			const { data: bulletinData, error: bulletinError } = await supabase
+				.from('ward-bulletin')
+				.update({ bulletin: contentToSave }) // Assuming 'bulletin' is the column you want to update.
+				.eq('id', 2)
+				// .eq('id', 6)
+				.select();
+
+			if (bulletinError) {
+				console.error('Error updating bulletin:', error);
+				setError(true);
+				return;
+			}
+		} catch (error) {
+			console.error('Error caught:', error);
+		}
+
+		setSaving(false);
+	};
+
+	// const insertData = async () => {
+	// 	const { data, error } = await supabase
+	// 		.from('ward-bulletin')
+	// 		.insert([{ bulletin: content, stake: 'Test', ward: 'test' }]);
+
+	// 	if (error) {
+	// 		console.error('Error inserting data:', error);
+	// 		setError(true);
+	// 		return;
+	// 	}
+	// };
+
+	return (
+		<div css={styles}>
+			<LoadingButton
+				variant="contained"
+				disabled={saving}
+				onClick={handleSave}
+				sx={{ margin: '12px 0', maxWidth: '80px', backgroundColor: '#4caf50' }}
+				loading={saving}
+				className="save-button"
+			>
+				Save
+			</LoadingButton>
+		</div>
+	);
+};
+
+export default SaveButton;
